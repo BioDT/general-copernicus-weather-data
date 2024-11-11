4f814fd93d5da4138c20d21952150ca96de80a97
Thomas Banitz <tb4micsoft@outlook.com>
2024-11-11 16:18:08 +0100

    implement suggested change in test syntax from pull request

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,36 +1,36 @@
 import pytest
 
-import copernicus.utils as ut
+from copernicus.utils import get_days_in_month, is_leap_year
 
 
 def test_is_leap_year():
     """Test leap year logic."""
-    assert ut.is_leap_year(2020)
-    assert not ut.is_leap_year(2019)
-    assert not ut.is_leap_year(1900)  # Century non-leap year
-    assert ut.is_leap_year(2000)  # Century leap year
+    assert is_leap_year(2020)
+    assert not is_leap_year(2019)
+    assert not is_leap_year(1900)  # Century non-leap year
+    assert is_leap_year(2000)  # Century leap year
 
 
 def test_get_days_in_month():
     """Test the number of days in each month for both leap and non-leap years."""
     # Test valid months
-    assert ut.get_days_in_month(2021, 1) == 31  # January
-    assert ut.get_days_in_month(2021, 2) == 28  # February (non-leap year)
-    assert ut.get_days_in_month(2020, 2) == 29  # February (leap year)
-    assert ut.get_days_in_month(2021, 3) == 31  # March
-    assert ut.get_days_in_month(2021, 4) == 30  # April
-    assert ut.get_days_in_month(2021, 5) == 31  # May
-    assert ut.get_days_in_month(2021, 6) == 30  # June
-    assert ut.get_days_in_month(2021, 7) == 31  # July
-    assert ut.get_days_in_month(2021, 8) == 31  # August
-    assert ut.get_days_in_month(2021, 9) == 30  # September
-    assert ut.get_days_in_month(2021, 10) == 31  # October
-    assert ut.get_days_in_month(2021, 11) == 30  # November
-    assert ut.get_days_in_month(2021, 12) == 31  # December
+    assert get_days_in_month(2021, 1) == 31  # January
+    assert get_days_in_month(2021, 2) == 28  # February (non-leap year)
+    assert get_days_in_month(2020, 2) == 29  # February (leap year)
+    assert get_days_in_month(2021, 3) == 31  # March
+    assert get_days_in_month(2021, 4) == 30  # April
+    assert get_days_in_month(2021, 5) == 31  # May
+    assert get_days_in_month(2021, 6) == 30  # June
+    assert get_days_in_month(2021, 7) == 31  # July
+    assert get_days_in_month(2021, 8) == 31  # August
+    assert get_days_in_month(2021, 9) == 30  # September
+    assert get_days_in_month(2021, 10) == 31  # October
+    assert get_days_in_month(2021, 11) == 30  # November
+    assert get_days_in_month(2021, 12) == 31  # December
 
     # Test invalid month
     with pytest.raises(ValueError):
-        ut.get_days_in_month(2021, 0)  # Invalid month (0)
+        get_days_in_month(2021, 0)  # Invalid month (0)
 
     with pytest.raises(ValueError):
-        ut.get_days_in_month(2021, 13)  # Invalid month (13)
+        get_days_in_month(2021, 13)  # Invalid month (13)
